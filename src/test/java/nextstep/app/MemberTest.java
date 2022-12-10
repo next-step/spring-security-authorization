beac23b4f25797fe8e942bb31e9fc713d24a0a16
jaylin.re <an.jin@croquis.com>
2022-12-10 16:32:18 +0900

    feat: MemberTest 동작하도록 basic 요청 시점에 session값 추가

--- src/test/java/nextstep/app/MemberTest.java
+++ src/test/java/nextstep/app/MemberTest.java
@@ -1,6 +1,8 @@
 package nextstep.app;
 
 import nextstep.security.authentication.Authentication;
+import nextstep.security.authentication.UsernamePasswordAuthentication;
+import nextstep.security.context.SecurityContext;
 import nextstep.security.context.SecurityContextHolder;
 import nextstep.app.domain.Member;
 import nextstep.app.infrastructure.InmemoryMemberRepository;
@@ -9,11 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Base64;
+import java.util.Collections;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -30,7 +35,9 @@ public class MemberTest {
 
     @Test
     void request_success_with_admin_user() throws Exception {
-        ResultActions response = requestWithBasicAuth(TEST_ADMIN_MEMBER.getEmail(), TEST_ADMIN_MEMBER.getPassword());
+        ResultActions response = requestWithBasicAuthAndSession(
+                TEST_ADMIN_MEMBER.getEmail(), TEST_ADMIN_MEMBER.getPassword(), TEST_ADMIN_MEMBER.getRoles()
+        );
 
         response.andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2));
@@ -42,9 +49,11 @@ public class MemberTest {
 
     @Test
     void request_fail_with_general_user() throws Exception {
-        ResultActions response = requestWithBasicAuth(TEST_USER_MEMBER.getEmail(), TEST_USER_MEMBER.getPassword());
+        ResultActions response = requestWithBasicAuthAndSession(
+                TEST_USER_MEMBER.getEmail(), TEST_USER_MEMBER.getPassword(), TEST_USER_MEMBER.getRoles()
+        );
 
-        response.andExpect(status().isForbidden());
+         response.andExpect(status().isForbidden());
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         assertThat(authentication.isAuthenticated()).isTrue();
@@ -53,24 +62,33 @@ public class MemberTest {
 
     @Test
     void request_fail_with_no_user() throws Exception {
-        ResultActions response = requestWithBasicAuth("none", "none");
+        ResultActions response = requestWithBasicAuthAndSession("none", "none", Collections.emptySet());
 
         response.andExpect(status().isUnauthorized());
     }
 
     @Test
     void request_fail_invalid_password() throws Exception {
-        ResultActions response = requestWithBasicAuth(TEST_ADMIN_MEMBER.getEmail(), "invalid");
+        ResultActions response = requestWithBasicAuthAndSession(TEST_ADMIN_MEMBER.getEmail(), "invalid", Collections.emptySet());
 
         response.andExpect(status().isUnauthorized());
     }
 
-    private ResultActions requestWithBasicAuth(String username, String password) throws Exception {
+    private ResultActions requestWithBasicAuthAndSession(String username, String password, Set<String> roles) throws Exception {
         String token = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
 
         return mockMvc.perform(get("/members")
                         .header("Authorization", "Basic " + token)
+                        .session(getLoginSession(username, password, roles))
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
         );
+    }
+
+    private MockHttpSession getLoginSession(String username, String password, Set<String> roles) {
+        MockHttpSession session = new MockHttpSession();
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(UsernamePasswordAuthentication.ofAuthenticated(username, password, roles));
+        session.setAttribute("SPRING_SECURITY_CONTEXT", context);
+        return session;
     }
 }
