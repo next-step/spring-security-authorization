e136e3ec31543777893b866ef35f5cef25587269
hyunssooo <jhs695@gmail.com>
2022-12-11 16:20:19 +0900

    fix: 인가 과정에서 인증 정보 불러오는 방법 수정

--- src/main/java/nextstep/security/authorization/AuthorizationFilter.java
+++ src/main/java/nextstep/security/authorization/AuthorizationFilter.java
@@ -1,14 +1,18 @@
 package nextstep.security.authorization;
 
 import java.io.IOException;
+import java.util.Optional;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import nextstep.security.authentication.Authentication;
 import nextstep.security.context.SecurityContext;
+import nextstep.security.context.SecurityContextHolder;
 import nextstep.security.context.SecurityContextRepository;
+import nextstep.security.exception.AuthenticationException;
 import nextstep.security.exception.AuthorizationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.GenericFilterBean;
@@ -29,16 +33,41 @@ public class AuthorizationFilter extends GenericFilterBean {
         ServletResponse response,
         FilterChain chain
     ) throws IOException, ServletException {
+        preAuthorization((HttpServletRequest) request);
         try {
-            final SecurityContext context = securityContextRepository.loadContext((HttpServletRequest) request);
-            if (context.getAuthentication().getAuthorities().stream().noneMatch(roleManager::hasRole)) {
+            final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null) {
+                throw new AuthenticationException();
+            }
+
+            if (authentication.getAuthorities().stream().noneMatch(roleManager::hasRole)) {
                 throw new AuthorizationException();
             }
+        } catch (AuthenticationException e) {
+            ((HttpServletResponse) response).sendError(
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase()
+            );
+            return;
         } catch (AuthorizationException e) {
-            ((HttpServletResponse) response).sendError(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN.getReasonPhrase());
+            ((HttpServletResponse) response).sendError(
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN.getReasonPhrase()
+            );
             return;
         }
 
         chain.doFilter(request, response);
+    }
+
+    private void preAuthorization(HttpServletRequest request) {
+        final SecurityContext context = (SecurityContext) request.getSession().getAttribute(
+            SecurityContextHolder.SPRING_SECURITY_CONTEXT_KEY
+        );
+
+        final Authentication authentication = Optional.ofNullable(context)
+            .map(SecurityContext::getAuthentication)
+            .orElseGet(() -> securityContextRepository.loadContext(request).getAuthentication());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
