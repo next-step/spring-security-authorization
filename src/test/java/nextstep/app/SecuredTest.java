a63a21ece56e2fa4c3aee6fa44afa581be31db86
parkSeryu <pjh971212@gmail.com>
2025-02-10 20:30:09 +0900

    test: 테스트 코드 구조화
    - testFixture 추가
    - 기존 테스트 코드 구조 변경
    - 사용하지 않는 테스트 클래스 제거

--- src/test/java/nextstep/app/SecuredTest.java
+++ src/test/java/nextstep/app/SecuredTest.java
@@ -1,7 +1,7 @@
 package nextstep.app;
 
-import nextstep.app.domain.Member;
 import nextstep.app.domain.MemberRepository;
+import nextstep.security.fixture.MemberTestFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,9 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Base64;
-import java.util.Set;
-
+import static nextstep.security.fixture.MemberTestFixture.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,8 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class SecuredTest {
-    private final Member TEST_ADMIN_MEMBER = new Member("a@a.com", "password", "a", "", Set.of("ADMIN"));
-    private final Member TEST_USER_MEMBER = new Member("b@b.com", "password", "b", "", Set.of());
 
     @Autowired
     private MockMvc mockMvc;
@@ -41,7 +37,7 @@ class SecuredTest {
     @DisplayName("ADMIN 권한을 가진 사용자가 요청할 경우 모든 회원 정보를 조회할 수 있다.")
     @Test
     void request_search_success_with_admin_user() throws Exception {
-        String token = Base64.getEncoder().encodeToString((TEST_ADMIN_MEMBER.getEmail() + ":" + TEST_ADMIN_MEMBER.getPassword()).getBytes());
+        String token = createAdminToken();
 
         ResultActions response = mockMvc.perform(get("/search")
                 .header("Authorization", "Basic " + token)
@@ -55,7 +51,7 @@ class SecuredTest {
     @DisplayName("일반 사용자가 요청할 경우 권한이 없어야 한다.")
     @Test
     void request_search_fail_with_general_user() throws Exception {
-        String token = Base64.getEncoder().encodeToString((TEST_USER_MEMBER.getEmail() + ":" + TEST_USER_MEMBER.getPassword()).getBytes());
+        String token = createMemberToken();
 
         ResultActions response = mockMvc.perform(get("/search")
                 .header("Authorization", "Basic " + token)
