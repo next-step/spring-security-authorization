0e91560b644feca7ed19df4a81903ca1952ae69d
haero77 <shineecard5@gmail.com>
2025-02-16 00:52:58 +0900

    (step2) 요청 패턴에 따라 적절한 인가 관리자를 호출하여 인가하도록 인가 로직 위임

--- src/main/java/nextstep/security/authorization/RequestAuthorizationManager.java
+++ src/main/java/nextstep/security/authorization/RequestAuthorizationManager.java
@@ -2,9 +2,9 @@ package nextstep.security.authorization;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.security.authentication.Authentication;
+import nextstep.security.util.matcher.RequestMatcherEntry;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * - 요청에 대해서 적합한 권한이 있는지만 판단.
@@ -12,32 +12,23 @@ import java.util.Set;
  */
 public class RequestAuthorizationManager implements AuthorizationManager<HttpServletRequest> {
 
-    private static final List<String> PUBLIC_URIS = List.of("/login", "/search");
+    private final List<RequestMatcherEntry<AuthorizationManager>> mappings;
+
+    public RequestAuthorizationManager(List<RequestMatcherEntry<AuthorizationManager>> mappings) {
+        this.mappings = mappings;
+    }
 
     @Override
     public AuthorizationDecision check(Authentication authentication, HttpServletRequest request) {
-        if (PUBLIC_URIS.contains(request.getRequestURI())) {
-            return AuthorizationDecision.grantedOf();
-        }
-
-        if (!isAuthenticated(authentication)) { // PUBLIC_URIS에 포함되지 않은 URI는 모두 인증 필요.
+        if (!isAuthenticated(authentication)) {
             return AuthorizationDecision.deniedOf();
         }
 
-        return checkAuthorityByUri(authentication, request.getRequestURI());
-    }
-
-    private AuthorizationDecision checkAuthorityByUri(Authentication authentication, String requestURI) {
-        if (requestURI.equals("/members/me")) { // 인증된 사용자 허용
-            return AuthorizationDecision.grantedOf();
-        }
-
-        if (requestURI.equals("/members")) { // ADMIN만 허용
-            Set<String> authorities = authentication.getAuthorities();
-            if (authorities.contains("ADMIN")) {
-                return AuthorizationDecision.grantedOf();
+        for (RequestMatcherEntry<AuthorizationManager> entry : mappings) {
+            if (entry.matches(request)) {
+                AuthorizationManager manager = entry.getEntry();
+                return manager.check(authentication, request);
             }
-            return AuthorizationDecision.deniedOf();
         }
 
         return AuthorizationDecision.deniedOf();
