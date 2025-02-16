b73d13679d403afa5dad13146bba7e1915d8ff28
evan <evan@danal.co.kr>
2025-02-16 09:39:01 +0900

    feat: filter Proxy 예외 처리 추가

---
+++ src/test/java/nextstep/fixture/MemberFixture.java
@@ -0,0 +1,36 @@
+package nextstep.fixture;
+
+import nextstep.app.domain.Member;
+
+import java.util.Set;
+
+public enum MemberFixture {
+    TEST_ADMIN_MEMBER(new Member(
+            "a@a.com", "password",
+            "a", "",
+            Set.of("USER", "ADMIN")
+    )),
+    TEST_USER_MEMBER(new Member(
+            "b@b.com", "password",
+            "b", "",
+            Set.of("USER")
+    ));
+
+    private final Member member;
+
+    MemberFixture(Member member) {
+        this.member = member;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public String getEmail() {
+        return member.getEmail();
+    }
+
+    public String getPassword() {
+        return member.getPassword();
+    }
+}
