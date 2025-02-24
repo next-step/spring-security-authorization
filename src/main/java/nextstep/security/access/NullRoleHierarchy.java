ba2b826bfed49c6b2e05ffbbe122f507380b12c1
djawnstj <djawnstj44@gmail.com>
2025-02-24 18:39:06 +0900

    refactor: NullRoleHierarchy 인덴트 수정

--- src/main/java/nextstep/security/access/NullRoleHierarchy.java
+++ src/main/java/nextstep/security/access/NullRoleHierarchy.java
@@ -4,8 +4,8 @@ import java.util.Collection;
 
 public final class NullRoleHierarchy implements RoleHierarchy {
 
-	@Override
-	public Collection<String> getReachableGrantedAuthorities(Collection<String> authorities) {
-		return authorities;
-	}
+    @Override
+    public Collection<String> getReachableGrantedAuthorities(Collection<String> authorities) {
+        return authorities;
+    }
 }
