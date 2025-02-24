e6c98bee528d58d6274644a1aa83d8f3c4958418
djawnstj <djawnstj44@gmail.com>
2025-02-24 18:39:42 +0900

    test: RoleHierarchyImpl 역할 순환 관계 테스트

--- src/test/java/nextstep/security/access/RoleHierarchyImplTest.java
+++ src/test/java/nextstep/security/access/RoleHierarchyImplTest.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RoleHierarchyImplTest {
 
@@ -29,4 +30,38 @@ class RoleHierarchyImplTest {
                 .contains("ROLE_ADMIN", "ROLE_MANAGER", "ROLE_USER");
     }
 
+    @DisplayName("하위 계층의 모든 역할을 순환 관계로 포함시킨다")
+    @Test
+    public void setHierarchyCircular() throws Exception {
+        // given
+        final RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+
+        // when
+        roleHierarchy.setHierarchy("""
+                ROLE_ADMIN > ROLE_MANAGER
+                ROLE_MANAGER > ROLE_USER
+                ROLE_USER > ROLE_ADMIN
+                """);
+
+        // then
+        final Collection<String> actual1 = roleHierarchy.getReachableGrantedAuthorities(List.of("ROLE_ADMIN"));
+        final Collection<String> actual2 = roleHierarchy.getReachableGrantedAuthorities(List.of("ROLE_MANAGER"));
+        final Collection<String> actual3 = roleHierarchy.getReachableGrantedAuthorities(List.of("ROLE_USER"));
+
+        assertAll(
+                () -> {
+                    assertThat(actual1).hasSize(3)
+                            .contains("ROLE_ADMIN", "ROLE_MANAGER", "ROLE_USER");
+                },
+                () -> {
+                    assertThat(actual2).hasSize(3)
+                            .contains("ROLE_ADMIN", "ROLE_MANAGER", "ROLE_USER");
+                },
+                () -> {
+                    assertThat(actual3).hasSize(3)
+                            .contains("ROLE_ADMIN", "ROLE_MANAGER", "ROLE_USER");
+                }
+        );
+
+    }
 }
