9075dc30beec8a20709ed17e9bb14479a8717aab
djawnstj <djawnstj44@gmail.com>
2025-02-25 18:53:04 +0900

    refactor: 인덴트 수정

--- src/test/java/nextstep/app/TestMemberController.java
+++ src/test/java/nextstep/app/TestMemberController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-    class TestMemberController {
+class TestMemberController {
 
-        private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-        public TestMemberController(final MemberRepository memberRepository) {
-            this.memberRepository = memberRepository;
-        }
-
-        @GetMapping("/members/managers")
-        public ResponseEntity<List<Member>> managers() {
-            List<Member> members = memberRepository.findAll()
-                    .stream().filter(member -> member.getRoles().contains("MANAGER"))
-                    .toList();
-            return ResponseEntity.ok(members);
-        }
+    public TestMemberController(final MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    @GetMapping("/members/managers")
+    public ResponseEntity<List<Member>> managers() {
+        List<Member> members = memberRepository.findAll()
+                .stream().filter(member -> member.getRoles().contains("MANAGER"))
+                .toList();
+        return ResponseEntity.ok(members);
+    }
+}
