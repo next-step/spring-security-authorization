a79edcb2178514c4f47d4ecf841948ad0aa459ef
pilming44 <pilming44@naver.com>
2025-03-01 20:56:23 +0900

    test: 순환참조 테스트코드 추가

--- src/test/java/nextstep/security/authorization/RoleHierarchyImplTest.java
+++ src/test/java/nextstep/security/authorization/RoleHierarchyImplTest.java
@@ -6,21 +6,22 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class RoleHierarchyImplTest {
     @Test
     @DisplayName("'>'구분자 없이 역할 계층구조 세팅시 예외발생")
     void exception_on_role_hierarchy_setup_without_delimiter() {
-        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            roleHierarchy.setHierarchy("ROLE_ADMIN,ROLE_USER");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl("ROLE_ADMIN,ROLE_USER");
         });
     }
 
     @Test
     @DisplayName("한줄로 여러개의 역할을 계층형으로 세팅")
     void setHierarchy_singleLine_multipleRoles() {
-        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        roleHierarchy.setHierarchy("ADMIN > USER > GUEST");
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl("ADMIN > USER > GUEST");
 
         // ADMIN 역할
         Set<String> adminReachable = roleHierarchy.getReachableRoleAuthorities("ADMIN");
@@ -33,5 +34,14 @@ class RoleHierarchyImplTest {
         // GUEST 역할
         Set<String> guestReachable = roleHierarchy.getReachableRoleAuthorities("GUEST");
         Assertions.assertEquals(Set.of("GUEST"), guestReachable);
+    }
+
+    @Test
+    @DisplayName("순환참조시 예외발생")
+    void circular_reference_throw_exception() {
+        // 순환참조가 있는 계층:
+        assertThrows(IllegalArgumentException.class, () -> {
+            new RoleHierarchyImpl("ADMIN > USER\nUSER > GUEST\nGUEST > ADMIN");
+        });
     }
 }
